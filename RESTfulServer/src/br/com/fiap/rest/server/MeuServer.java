8f14e98cfea2db56d350d4d89b60adb40b404600
Fernando Santiago <fspoliveira@yahoo.com.br>
2012-05-30 14:48:38 -0300

    Hello World

---
+++ RESTfulServer/src/br/com/fiap/rest/server/MeuServer.java
@@ -0,0 +1,48 @@
+package br.com.fiap.rest.server;
+
+import java.util.Set;
+
+import org.restlet.Server;
+import org.restlet.data.Form;
+import org.restlet.data.Protocol;
+import org.restlet.data.Status;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.resource.Get;
+import org.restlet.resource.Post;
+import org.restlet.resource.ServerResource;
+
+
+
+public class MeuServer extends ServerResource {
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		new Server(Protocol.HTTP, 8182, MeuServer.class).start();
+	}
+
+	@Get
+	public String toString() {  
+	     return "hello, world"; 
+	}
+	
+	public boolean allowPost(){
+		return true;
+	}
+	
+	@Post
+	public void fazPost (Representation rep){
+		Form form = new Form(rep);
+		System.out.println("Executei o POST");
+		System.out.println("Names: "+ form.getNames());
+		Representation represposta = new StringRepresentation("Sucesso");
+		getResponse().setStatus(Status.SUCCESS_ACCEPTED);
+		getResponse().setEntity(represposta);
+	}
+	
+
+
+}
