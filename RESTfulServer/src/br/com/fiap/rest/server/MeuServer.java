832040936c4ff29c48e698096b410822c54ffcf1
Fernando Santiago <fspoliveira@yahoo.com.br>
2012-05-30 15:51:10 -0300

    End

--- RESTfulServer/src/br/com/fiap/rest/server/MeuServer.java
+++ RESTfulServer/src/br/com/fiap/rest/server/MeuServer.java
@@ -1,16 +1,22 @@
 package br.com.fiap.rest.server;
 
+import java.io.IOException;
 import java.util.Set;
 
+import org.json.JSONException;
 import org.restlet.Server;
 import org.restlet.data.Form;
 import org.restlet.data.Protocol;
 import org.restlet.data.Status;
+import org.restlet.ext.json.JsonConverter;
+import org.restlet.ext.json.JsonRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.representation.StringRepresentation;
 import org.restlet.resource.Get;
 import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
+
+import br.com.fiap.bean.Contato;
 
 
 
@@ -34,13 +40,19 @@ public class MeuServer extends ServerResource {
 	}
 	
 	@Post("json")
-	public void fazPost (Representation rep){
+	public void fazPost (Representation rep) throws IOException, JSONException{
 		Form form = new Form(rep);
 		System.out.println("Executei o POST");
 		System.out.println("Names: "+ form.getNames());
 		Representation represposta = new StringRepresentation("Sucesso");
 		getResponse().setStatus(Status.SUCCESS_ACCEPTED);
 		getResponse().setEntity(represposta);
+		
+		JsonConverter jc = new JsonConverter();
+		//Contato c = jc.toObject(rep,Contato.class,null);
+		
+		//System.out.println(c.getEmail());
+		
 	}
 	
 
