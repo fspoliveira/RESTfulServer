e94517f96b9570f26fe3ebc65f71c94c57664c08
Fernando Santiago <fspoliveira@yahoo.com.br>
2012-05-29 21:56:38 -0300

    Ajuste Bean, ajustes Libs e Generic Dao Funcionando

--- RESTfulServer/src/br/com/fiap/test/TestGenericDao.java
+++ RESTfulServer/src/br/com/fiap/test/TestGenericDao.java
@@ -1,9 +1,7 @@
 package br.com.fiap.test;
 
 import br.com.fiap.bean.Contato;
-import br.com.fiap.bean.Email;
-import br.com.fiap.bean.Logradouro;
-import br.com.fiap.bean.Telefone;
+
 import br.com.fiap.dao.ContatoDAO;
 
 public class TestGenericDao {
@@ -11,17 +9,9 @@ public class TestGenericDao {
 	public static void main(String args[]) {
 
 		ContatoDAO contatoDAO = new ContatoDAO();
-		Contato contato = new Contato();
-		Email email = new Email("fspoliveira@yahoo.com.br");
-		Logradouro logradouro = new Logradouro("Rua dos Perdidos, 10","São Paulo", "SP","Brasil");
-		Telefone telefone = new Telefone("7070 7070");
+		Contato contato = new Contato();		
 		
 		contato.setEmail("fspoliveira@yahoo.com.br");
-
-		//contato.setEmail(email);
-		//contato.setLogradouro(logradouro);
-	//	contato.setTelefone(telefone);
-
 		contatoDAO.insert(contato);
 		
 		System.out.println(contatoDAO.list());
